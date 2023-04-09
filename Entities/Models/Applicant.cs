92478889b99d9f4caf8b7f8ced6aec1b07a0e3c4
John <jcsay21@gmail.com>
2023-04-09 22:05:03 +0800

    Add DisplayName property

--- Entities/Models/Applicant.cs
+++ Entities/Models/Applicant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,34 @@ namespace Entities.Models
 {
     public class Applicant
     {
+        [DisplayName("FirstName")]
         public string? FirstName { get; set; }
+
+        [DisplayName("LastName")]
         public string? LastName { get; set; }
+
+        [DisplayName("Email")]
         public string? Email { get; set; }
+
+        [DisplayName("PhoneNumber")]
         public string? PhoneNumber { get; set; }
+
+        [DisplayName("BusinessNumber")]
         public int BusinessNumber { get; set; }
+
+        [DisplayName("LoanAmount")]
         public int LoanAmount { get; set; }
+
+        [DisplayName("CitizenshipStatus")]
         public string? CitizenshipStatus { get; set; }
+
+        [DisplayName("TimeTrading")]
         public int TimeTrading { get; set; }
+
+        [DisplayName("CountryCode")]
         public string? CountryCode { get; set; }
+
+        [DisplayName("Industry")]
         public string? Industry { get; set; }
     }
 }
